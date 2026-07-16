c2a947e2e052dbc3536645f74844943b83d18e6d
Kouamojp <kouamojp@gmail.com>
2026-07-16 14:42:13 -0400

    Hide few menu

--- src/components/sidebar/sidebarData.ts
+++ src/components/sidebar/sidebarData.ts
@@ -120,7 +120,7 @@ export const yatServicesItems: MenuSectionItem[] = [
     badge: 'New',
     badgeColor: 'bg-emerald-500 text-white',
   },
-  {
+  /* {
     label: 'sidebar.yatEvents',
     description: 'sidebar.yatEvents.desc',
     icon: CalendarDays,
@@ -133,7 +133,7 @@ export const yatServicesItems: MenuSectionItem[] = [
     icon: GraduationCap,
     path: '/learning',
     iconColor: 'text-violet-500',
-  },
+  }, */
   {
     label: 'sidebar.organizations',
     description: 'sidebar.organizations.desc',
@@ -186,7 +186,7 @@ export const yatServicesItems: MenuSectionItem[] = [
 
 // Communities & Content
 export const servicesItems: MenuSectionItem[] = [
-  {
+ /*  {
     label: 'sidebar.live',
     description: 'sidebar.live.desc',
     icon: Radio,
@@ -224,7 +224,7 @@ export const servicesItems: MenuSectionItem[] = [
     icon: ShoppingBag,
     path: '/yat-coin',
     iconColor: 'text-yellow-500',
-  },
+  }, */
   {
     label: 'sidebar.yatMarketplace',
     description: 'sidebar.yatMarketplace.desc',
